73fc766506cecb9f5863082833a6fa3cbf1c435b
miyazaki <miyazaki@keystone-net.com>
2024-07-12 16:54:38 +0900

    3-3

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,22 @@
 import styles from "./page.module.css";
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>テクノロジーの力で世界を変える</h1>
-      <p className={styles.description}>
-        私たちは市場をリードしているグルーバルテックカンパニーです。です。
-      </p>
-    </div>
+    <section className={styles.top}>
+      <div>
+        <h1 className={styles.title}>テクノロジーの力で世界を変える</h1>
+        <p className={styles.description}>
+          私たちは市場をリードしているグルーバルテックカンパニーです。
+        </p>
+      </div>
+      <Image
+        className={styles.bgimg}
+        src="/img-mv.jpg"
+        alt=""
+        width={4000}
+        height={1200}
+      ></Image>
+    </section>
   );
 }
